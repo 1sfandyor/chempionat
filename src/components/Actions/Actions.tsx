edb2aa68621bbf0e2515a3dffd335b9625ab216b
1sfandyor <isfandiyor06112000@gmail.com>
2024-07-17 10:12:03 +0500

    remove background

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -11,9 +11,9 @@ export const Actions = () => {
   
 
   return (
-    <div className="w-1/2 flex items-start justify-center bg-green-300 container mx-auto">
+    <div className="w-1/2 flex items-start justify-center container mx-auto pt-[19px]">
       <Image className="2xl:mr-[82.91px]" src={logo} alt="Logo" width={36} height={36}/>
-      <div className="flex flex-col h-full bg-blue-300 w-[393px]"> 
+      <div className="flex flex-col h-full w-[393px]"> 
         <div className="mb-[20px]">
           <Button button={{
                     className: 'bg-black rounded-[65px] text-white gap-10 mr-[3px]',
@@ -34,14 +34,8 @@ export const Actions = () => {
           }} />
         </div>
         
-        <div className="mb-[20px]">
-          <Button button={{
-                    className: '',
-                    spanClasses: 'text-[15px] font-medium text-black/40 leading-normal',
-                    onClick: () => setOpenLoginModal(true),
-                    text: 'Категории спорта'
-                    
-          }} />
+        <div className="mb-[20px] flex items-center ">
+          <p className="text-[15px] mr-16 font-medium text-black/40 leading-normal">Категории спорта</p>
 
           <Button button={{
                     className: ``,
