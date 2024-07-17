ebd8d8bb78f4c1eefd7901a958512a93cc114708
1sfandyor <isfandiyor06112000@gmail.com>
2024-07-17 10:11:49 +0500

    remove background and margin top

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Actions } from "@/components/Actions/Actions";
 import { HomeSection } from "@/components/Home/HomeSection";
 
 export default function Home() {
-  return <main className="w-full text-black flex bg-red-300 min-h-screen pt-[19px]">
+  return <main className="w-full text-black flex min-h-screen">
     <Actions/>
     <HomeSection/>
     </main>
