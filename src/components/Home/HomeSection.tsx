0a70e8081e0274c3bdd1c50848e02cca4465afe2
1sfandyor <isfandiyor06112000@gmail.com>
2024-07-17 10:12:47 +0500

    Fix styles

--- src/components/Home/HomeSection.tsx
+++ src/components/Home/HomeSection.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const HomeSection = () => {
   return (
-    <section className='w-1/2 bg-slate-500'>
+    <section className='w-1/2 pt-[19px] container border-s-[1px] border-s-slate-500'>
+      <div className='w-[382px] mx-auto'>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quasi necessitatibus reiciendis excepturi unde quas cum minus, sapiente iusto sunt laborum non iste veritatis aut ex doloremque perferendis. Nemo, quam?
+      </div>
+
     </section>
   )
 }
