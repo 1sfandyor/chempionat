09854f9b03464524d7dca5d2506181bf081c2235
1sfandyor <isfandiyor06112000@gmail.com>
2024-07-18 18:30:47 +0500

    pre-fix

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -15,17 +15,17 @@ export const Actions = () => {
   const [email, setEmail] = useState<string | null>(null);
   const [pin, setPin] = useState<string | null>(null);
 
-  const storedNickname = localStorage.getItem('nickname');
-  const storedEmail = localStorage.getItem('email');
-  const storedPin = localStorage.getItem('pincode');
-  
-  useEffect(() => {
-      setNickname(storedNickname);      
-      setEmail(storedEmail);
-      setPin(storedPin);
+  const storedNickname = window?.localStorage?.getItem('nickname');
 
-  }, [storedNickname, storedEmail, storedPin]);
-  
+
+  useEffect(() => {
+    const storedEmail = window.localStorage.getItem('email');
+    const storedPin = window.localStorage.getItem('pincode');
+
+    setNickname(storedNickname);
+    setEmail(storedEmail);
+    setPin(storedPin);
+  }, [storedNickname]);
 
   const modalItem: RegisterModalType = {
     text: email === null
@@ -36,47 +36,47 @@ export const Actions = () => {
     children: '',
     setOpenRegisterModal: () => setOpenRegisterModal(false)
   };
-  
+
   return (
     <>
       <div className="w-1/2 flex items-start justify-center container mx-auto pt-[19px]">
-        <Image className="2xl:mr-[82.91px]" src={logo} alt="Logo" width={36} height={36}/>
+        <Image className="2xl:mr-[82.91px]" src={logo} alt="Logo" width={36} height={36} />
         {
-          nickname 
-          ? <UserActions/>
-          : <div className="flex flex-col h-full w-[393px]"> 
+          nickname
+            ? <UserActions />
+            : <div className="flex flex-col h-full w-[393px]">
               <div className="mb-[20px]">
                 <Button button={{
-                          className: 'bg-black rounded-[65px] text-white gap-10 mr-[3px]',
-                          spanClasses: 'text-[15px] font-semibold leading-normal',
-                          onClick: () => setOpenLoginModal(true),
-                          px: 'px-[22px]',
-                          py: 'py-[15px]',
-                          text: 'вход',
-                          type: 'button'
-                        }} />
-      
+                  className: 'bg-black rounded-[65px] text-white gap-10 mr-[3px]',
+                  spanClasses: 'text-[15px] font-semibold leading-normal',
+                  onClick: () => setOpenLoginModal(true),
+                  px: 'px-[22px]',
+                  py: 'py-[15px]',
+                  text: 'вход',
+                  type: 'button'
+                }} />
+
                 <Button button={{
-                          className: `bg-gray rounded-[65px] gap-10`,
-                          spanClasses: `text-[15px] text-black  font-semiBold leading-normal ${SFPro.variable}`,
-                          onClick: () => setOpenRegisterModal(true),
-                          px: 'px-[22px]',
-                          py: 'py-[15px]',
-                          text: 'регистрация',
-                          type: 'button'
-                        }} />
+                  className: `bg-gray rounded-[65px] gap-10`,
+                  spanClasses: `text-[15px] text-black  font-semiBold leading-normal ${SFPro.variable}`,
+                  onClick: () => setOpenRegisterModal(true),
+                  px: 'px-[22px]',
+                  py: 'py-[15px]',
+                  text: 'регистрация',
+                  type: 'button'
+                }} />
               </div>
-              
+
               <div className="mb-[20px] flex items-center">
                 <p className="text-[15px] mr-16 font-medium text-black/40 leading-normal">Категории спорта</p>
-      
+
                 <Button button={{
-                          className: ``,
-                          spanClasses: `text-[15px] text-black  font-medium text-black/40 leading-normal ${SFPro.variable}`,
-                          onClick: () => setOpenRegisterModal(true),
-                          text: 'добавить',
-                          type: 'button'
-                        }} />
+                  className: ``,
+                  spanClasses: `text-[15px] text-black  font-medium text-black/40 leading-normal ${SFPro.variable}`,
+                  onClick: () => setOpenRegisterModal(true),
+                  text: 'добавить',
+                  type: 'button'
+                }} />
               </div>
             </div>
         }
@@ -87,7 +87,7 @@ export const Actions = () => {
           <>
             <div className="fixed top-0 left-0 w-full h-full bg-black/40" onClick={() => setOpenRegisterModal(false)}></div>
             <RegisterModal modal="register" modalItem={modalItem} />
-          </> 
+          </>
         )
       }
 
@@ -96,7 +96,7 @@ export const Actions = () => {
           <>
             <div className="fixed top-0 left-0 w-full h-full bg-black/40" onClick={() => setOpenLoginModal(false)}></div>
             <RegisterModal modal="login" modalItem={modalItem} />
-          </> 
+          </>
         )
       }
     </>
